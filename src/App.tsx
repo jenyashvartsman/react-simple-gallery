bf128c8de5a8fc8e4acfa23773977d92431cffc0
Jenya Shvartsman <jenyas@1touch.io>
2025-07-28 16:14:21 -0400

    implement sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Gallery from "./components/Gallery";
 import Sidebar from "./components/Sidebar";
+import { galleries } from "./data/galleries.data";
 
 function App() {
+  const [selectedGalleryIndex, setSelectedGalleryIndex] = useState<number>(0);
+
   return (
     <div className="app">
       <aside className="app__sidebar">
-        <Sidebar />
+        <Sidebar
+          galleries={galleries}
+          selectedGalleryIndex={selectedGalleryIndex}
+          galleryClick={(index) => {
+            setSelectedGalleryIndex(index);
+          }}
+        />
       </aside>
 
       <main className="app__main">
+        content
         <Gallery />
       </main>
     </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,37 @@
-const Sidebar = () => {
-  return <div className="sidebar">{/* Sidebar content goes here */}</div>;
+import "./Sidebar.css";
+import type { GalleryDto } from "../dtos/gallery.dto";
+
+interface SidebarProps {
+  galleries: GalleryDto[];
+  selectedGalleryIndex: number;
+  galleryClick: (index: number) => void;
+}
+
+const Sidebar = ({
+  galleries,
+  selectedGalleryIndex,
+  galleryClick,
+}: SidebarProps) => {
+  return (
+    <ul className="sidebar">
+      {galleries.map((gallery, galleryIndex) => (
+        <li
+          key={gallery.title}
+          className={`sidebar__item ${
+            selectedGalleryIndex === galleryIndex ? "sidebar__item--active" : ""
+          }`}
+          onClick={() => galleryClick(galleryIndex)}
+        >
+          <h3 className="sidebar__title">{gallery.title}</h3>
+          <img
+            src={gallery.coverImage.url}
+            alt={gallery.title}
+            className="sidebar__thumbnail"
+          />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Sidebar;
